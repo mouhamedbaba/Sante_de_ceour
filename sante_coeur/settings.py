d1419b2ee7b8ad45bc9373388f2c6a46caf67dc7
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-03-02 21:06:38 +0000

    fix apostroph prohib csrf token

--- sante_coeur/settings.py
+++ sante_coeur/settings.py
@@ -24,7 +24,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sante-de-coeur.vercel.app", "http://localhost:8000"
 ]
 
-CSRF_COOKIE_DOMAIN = "'sante-de-coeur.vercel.app" 
+CSRF_COOKIE_DOMAIN = "sante-de-coeur.vercel.app" 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
